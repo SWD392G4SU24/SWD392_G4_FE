e8f5dc9f40bf962af1231673a68c0e934d5bdc25
Always-smile-in-trouble <leanhduy16022002@gmail.com>
2024-06-04 00:00:44 +0700

    update login and regis

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -16,7 +16,7 @@ function Login1() {
         token: "aaaaaaa",
       });
       dispatch(login(res.data));
-      dispatch(logout());
+      // dispatch(logout());
     } catch (error) {
       console.log(error);
     }
@@ -56,13 +56,13 @@ function Login1() {
               <input
                 type="email"
                 className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none
-              dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer "
+              dark:focus:border-orange-200 focus:outline-none focus:ring-0 focus:text-white focus:border-orange-200 peer "
                 placeholder=""
               />
               <label
                 htmlFor=""
                 className="absolute text-sm text-white duration-300 transform -translate-y-1 scale-75 top-3 -z-10 origin-[0]
-            peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
+            peer-focus:left-0 peer-focus:text-orange-300 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
             peer-focus:scale-75 peer-focus:-translate-y-6 "
               >
                 Your Password
@@ -86,7 +86,7 @@ function Login1() {
               Login
             </button>
             <div>
-              <span className="mt-4">
+              <span className="mt-4 px-10">
                 New Here?
                 <Link to="/Register" className="text-black">
                   Create an Account
--- src/component/register/regis.jsx
+++ src/component/register/regis.jsx
@@ -6,7 +6,7 @@ function Register() {
   return (
     <div>
       <div
-        className="text-white h-[100vh] w-screen flex justify-center items-center bg-cover"
+        className="text-white h-[100vh] w-full flex justify-center items-center bg-cover"
         style={{
           backgroundImage: "url('../src/assets/website/bg-login1.jpg')",
         }}
@@ -37,13 +37,13 @@ function Register() {
               <input
                 type="email"
                 className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none
-            dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer "
+            dark:focus:border-orange-200 focus:outline-none focus:ring-0 focus:text-white focus:border-orange-200 peer "
                 placeholder=""
               />
               <label
                 htmlFor=""
                 className="absolute text-sm text-white duration-300 transform -translate-y-1 scale-75 top-3 -z-10 origin-[0]
-          peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
+          peer-focus:left-0 peer-focus:text-orange-300 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
           peer-focus:scale-75 peer-focus:-translate-y-6 "
               >
                 Your Password
@@ -54,13 +54,13 @@ function Register() {
               <input
                 type="email"
                 className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none
-            dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer "
+            dark:focus:border-orange-200 focus:outline-none focus:ring-0 focus:text-white focus:border-orange-200 peer "
                 placeholder=""
               />
               <label
                 htmlFor=""
                 className="absolute text-sm text-white duration-300 transform -translate-y-1 scale-75 top-3 -z-10 origin-[0]
-          peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
+          peer-focus:left-0 peer-focus:text-orange-300 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 
           peer-focus:scale-75 peer-focus:-translate-y-6 "
               >
                 Confirm Password
