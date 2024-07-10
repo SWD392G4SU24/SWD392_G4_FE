f09cf54a35e6d51370628f387e039439958aa4b9
khangkt03 <khanglunmh@yahoo.com>
2024-07-10 15:03:32 +0700

    testApiProfile

--- src/Page/Profile/Profile1.jsx
+++ src/Page/Profile/Profile1.jsx
@@ -1,137 +1,26 @@
 /* eslint-disable no-unused-vars */
-// /* eslint-disable no-unused-vars */
-// import React from 'react'
-// import api from '../../config/axios'
-
-
-// function Profile1() {
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-
-// export default Profile1
-
-// import React, { useEffect, useState } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { selectUser } from '../../redux/features/counterSlice'
-// import api from '../../config/axios'
-
-// function Profile1() {
-//   const dispatch = useDispatch()
-//   const user = useSelector(selectUser)
-//   const [userInfo, setUserInfo] = useState(null)
-
-//   useEffect(() => {
-//     const fetchUserInfo = async () => {
-//       try {
-//         const response = await api.get('/user/pagination')
-//         setUserInfo(response.data.data[0])
-//       } catch (error) {
-//         console.error('Lỗi khi lấy thông tin người dùng:', error)
-//       }
-//     }
-
-//     if (user) {
-//       fetchUserInfo()
-//     }
-//   }, [user])
-
-//   return (
-//     <div>
-//       {userInfo ? (
-//         <div>
-//           <h2>Thông tin cá nhân</h2>
-//           <p>Họ và tên: {userInfo.fullName}</p>
-//           <p>Email: {userInfo.email}</p>
-//           <p>Số điện thoại: {userInfo.phoneNumber}</p>
-//           <p>Địa chỉ: {userInfo.address}</p>
-//           <p>Điểm: {userInfo.point}</p>
-//         </div>
-//       ) : (
-//         <p>Đang tải thông tin người dùng...</p>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default Profile1
-
-/* eslint-disable no-unused-vars */
-// import React, { useEffect, useState } from 'react';
-// import api from '../../config/axios';
-
-// function Profile1() {
-//   const [user, setUser] = useState(null);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchUserProfile = async () => {
-//       try {
-//         const response = await api.get('/user/pagination'); // Thay đổi endpoint API cho phù hợp
-//         Headers.
-//         setUser(response.data);
-//       } catch (err) {
-//         setError(err);
-//       }
-//     };
-
-//     fetchUserProfile();
-//   }, []);
-
-//   if (error) {
-//     return <div>Đã xảy ra lỗi: {error.message}</div>;
-//   }
-
-//   if (!user) {
-//     return <div>Đang tải...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>Thông tin tài khoản</h1>
-//       <p>Biệt danh: {user.fullName}</p>
-//       <p>Email: {user.email}</p>
-//       <p>Số điện thoại: {user.phoneNumber}</p>
-//       <p>Địa chỉ: {user.address}</p>
-//       <p>Điểm: {user.point}</p>
-//     </div>
-//   );
-// }
-
-// export default Profile1;
-
-
 
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser, selectToken, selectId, selectUserName } from "../../redux/features/counterSlice";
 import api from '../../config/axios';
 
 function Profile1() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const userInfo = useSelector(selectUser);
+  const token = useSelector(selectToken);
+  const id = useSelector(selectId);
+
 
   useEffect(() => {
     const fetchUserProfile = async () => {
       try {
-        // Retrieve token and ID from localStorage
-        const token = localStorage.getItem('token');
-        const id = localStorage.getItem('id');
-
+     
         // Make API call with token and ID
-        const response = await api.get(`/user/customer-pagination?id=${id}`, {
-          headers: {
-            Authorization: `${token}`,
-          },
-        });
-
-        // Check if the ID from storage matches the ID from API response
-        if (response.data.id !== id) {
-          throw new Error('User ID does not match');
-        }
-
+        const response = await api.get(`/user-current`);
         setUser(response.data); // Assuming API returns user data directly
+        
       } catch (err) {
         setError(err);
       }
@@ -144,20 +33,18 @@ function Profile1() {
     return <div>Đã xảy ra lỗi: {error.message}</div>;
   }
 
-  if (!user) {
-    return <div>Đang tải...</div>;
-  }
 
   return (
     <div>
       <h1>Thông tin tài khoản</h1>
-      <p>Biệt danh: {user.fullName}</p>
+      {/* <p>Biệt danh: {user.fullName}</p>
       <p>Email: {user.email}</p>
       <p>Số điện thoại: {user.phoneNumber}</p>
       <p>Địa chỉ: {user.address}</p>
-      <p>Điểm: {user.point}</p>
+      <p>Điểm: {user.point}</p> */}
     </div>
   );
 }
 
 export default Profile1;
+
