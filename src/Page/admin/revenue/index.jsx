33a4703f4e02d22de3b922b75d04e3d17c093530
tlinh-something <linhntse171620@fpt.edu.vn>
2024-07-12 09:59:41 +0700

    update dashboard

--- src/Page/admin/revenue/index.jsx
+++ src/Page/admin/revenue/index.jsx
@@ -1,76 +1,138 @@
 import { DownOutlined } from "@ant-design/icons";
-import { BarChart, LineChart } from "@mui/x-charts";
-import { Avatar, Segmented } from "antd";
+// import { BarChart, LineChart } from "@mui/x-charts";
+import { Avatar, Segmented, Table } from "antd";
 import "./index.scss";
-import { data } from "autoprefixer";
-import { Bar } from "react-chartjs-2";
-// import React, { useEffect, useState } from "react";
-// import api from "../../../config/axios";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+// import { data } from "autoprefixer";
+// import { Bar } from "react-chartjs-2";
+import React, { useEffect, useState } from "react";
+import api from "../../../config/axios";
 
 function Revenue() {
-  //   const [revenues, setRevenue] = useState([]);
+  const [revenues, setRevenue] = useState([]);
+  const [tag, setTag] = useState([]);
 
-  //   const fetchDataRevenue = async () => {
-  //     const rs = await api.get(`/revenue/get-data`); //api revenue cua Nam
-  //     setRevenue(rs.data);
-  //   };
-
-  //   useEffect(() => {
-  //     fetchDataRevenue();
-  //   });
-
-  const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-  ];
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-          "rgba(255, 205, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(201, 203, 207, 0.2)",
-        ],
-        borderColor: [
-          "rgb(255, 99, 132)",
-          "rgb(255, 159, 64)",
-          "rgb(255, 205, 86)",
-          "rgb(75, 192, 192)",
-          "rgb(54, 162, 235)",
-          "rgb(153, 102, 255)",
-          "rgb(201, 203, 207)",
-        ],
-        borderWidth: 1,
-      },
-    ],
+  const fetchDataRevenue = async () => {
+    // const rs = await api.get(`/revenue/get-data`); //api revenue cua Nam
+    const rs = await api.get(
+      `https://667cd2303c30891b865dc8d6.mockapi.io/revenue`
+    ); //api revenue cua Nam
+    setRevenue(rs.data);
+    console.log(rs.data);
   };
 
-  const config = {
-    type: "bar",
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
+  useEffect(() => {
+    fetchDataRevenue();
+  }, []);
+
+  const dataRevenue = [
+    {
+      name: "01",
+      revenue: 4000,
     },
-  };
+    {
+      name: "05",
+      revenue: 3000,
+    },
+    {
+      name: "10",
+      revenue: 2000,
+    },
+    {
+      name: "15",
+      revenue: 2780,
+    },
+    {
+      name: "20",
+      revenue: 1890,
+    },
+    {
+      name: "25",
+      revenue: 2390,
+    },
+    {
+      name: "30",
+      revenue: 3490,
+    },
+  ];
+
+  const data = [
+    {
+      name: "T1",
+      revenue: 4000,
+    },
+    {
+      name: "T2",
+      revenue: 2800,
+    },
+    {
+      name: "T3",
+      revenue: 1890,
+    },
+    {
+      name: "T4",
+      revenue: 3500,
+    },
+    {
+      name: "T5",
+      revenue: 2900,
+    },
+    {
+      name: "T6",
+      revenue: 4500,
+    },
+    {
+      name: "T7",
+      revenue: 3490,
+    },
+    {
+      name: "T8",
+      revenue: 2790,
+    },
+    {
+      name: "T9",
+      revenue: 2990,
+    },
+    {
+      name: "T10",
+      revenue: 3890,
+    },
+    {
+      name: "T11",
+      revenue: 4090,
+    },
+    {
+      name: "T12",
+      revenue: 2990,
+    },
+  ];
+
+  const column = [
+    {
+      title: "Kỳ",
+      dataIndex: "month",
+    },
+    {
+      title: "Lượt bán",
+      dataIndex: "totalRevenue",
+      align: "center",
+      sorter: (a, b) => a.totalRevenue - b.totalRevenue,
+    },
+  ];
 
   return (
-    <div>
+    <div className="pb-10">
       <div className="flex w-11/12">
         <h2>JeWellry Dashboard</h2>
         <div>
@@ -82,29 +144,81 @@ function Revenue() {
 
       <Segmented
         options={["Revenue", "Staff", "Counter"]}
-        onChange={(value) => console.log(value)}
+        onChange={(value) => {
+          setTag(value);
+          console.log(value);
+        }}
       />
 
-      <div className="shadow border w-11/12 block m-auto mt-10 p-5">
-        <h3>Tổng lợi nhuận</h3>
-        <LineChart
-          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-          series={[
-            {
-              data: [2, 5.5, 2, 8.5, 1.5, 5],
-              curve: "linear",
-            },
-          ]}
-          width={1000}
-          height={300}
-        />
-      </div>
-
-      <div>
+      {tag === "Revenue" ? (
         <div>
-          <Bar data={data} />
+          <div className="shadow border w-11/12 block m-auto mt-10 p-5">
+            <h3 className="pb-3 font-bold">Tổng lợi nhuận trong tháng</h3>
+
+            <ResponsiveContainer width="100%" height={300} className="text-sm">
+              <LineChart
+                width={500}
+                height={200}
+                data={dataRevenue}
+                syncId="anyId"
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Line
+                  type="monotone"
+                  dataKey="revenue"
+                  stroke="#000"
+                  fill="#000"
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <div className="flex justify-evenly pl-28 gap-16 pb-5">
+            <div className="shadow border mt-10 w-5/12 p-5">
+              <h3 className="font-bold">Thống kê số lượt bán theo từng kỳ</h3>
+              <Table
+                dataSource={revenues}
+                columns={column}
+                pagination={false}
+                size="small"
+                className="p-5"
+              />
+            </div>
+
+            <div className="shadow border mt-10 w-full mr-28 h-fit p-5">
+              <h3 className="font-bold">
+                Sơ đồ cột tổng quan từng tháng trong năm
+              </h3>
+              <ResponsiveContainer width="100%" height={544} className="pt-5">
+                <BarChart
+                  data={data}
+                  margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+                  barSize={30}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="revenue" fill="#000" />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>{" "}
         </div>
-      </div>
+      ) : (
+        <div>
+          <p>helooooo</p>
+        </div>
+      )}
     </div>
   );
 }
